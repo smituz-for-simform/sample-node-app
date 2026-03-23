98b80e23b85a2b2eb786d4c602d7d2b31e49c2c0
Smit Bhansali <smit.bhansali@simformsolutions.com>
2026-03-23 05:59:56 +0000

    feat: Updated Code aka broken code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const http = require('http');
+//const http = require('http');
 
-const port = 8080;
+//const port = 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
